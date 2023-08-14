83e3b249ccecbf26054ad7d74828279577aff64c
BA10432CB <prest.0808.bass@gmail.com>
2023-08-14 13:15:42 +0900

    1章

---
+++ programming/test/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 3;
+        b = 5;
+        c = a * b;
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+    }
+}
