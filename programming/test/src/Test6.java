4d49242f07d0bc49396ab44069d044c944a68510
BA10432CB <prest.0808.bass@gmail.com>
2023-08-20 12:34:57 +0900

    6章途中

---
+++ programming/test/src/Test6.java
@@ -0,0 +1,9 @@
+import comment.Zenhan;
+public class Test6 {
+    public static void main(String[] args) throws Exception {
+        Zenhan.doWarusa();
+        Zenhan.doTomage();
+        Kouhan.callDeae();
+        Kouhan.showMondokoro();
+    }
+}
