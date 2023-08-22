0ab89157e05001b5ec438b4ba90533f36c5a663a
BA10432CB <prest.0808.bass@gmail.com>
2023-08-22 19:25:30 +0900

    10章

--- programming/test/src/Hero.java
+++ programming/test/src/Hero.java
@@ -1,35 +1,37 @@
 public class Hero {
-    String name;
-    int hp;
-    Sword sword;
-    public void attack() {
-        System.out.println("this.name" + "は、攻撃した！");
-        System.out.println("敵に5ポイントのダメージをあたえた！");
+    String name = "ミナト";
+    int hp = 100;
+    // Sword sword;
+    public void attack(Matango m) {
+        System.out.println("this.name" + "の、攻撃！");
+        m.hp -= 5;
+        System.out.println("5ポイントのダメージをあたえた！");
     }
-    public void sleep() {
-        this.hp = 100;
-        System.out.println(this.name + "は、眠って回復した！");
-    }
-    public void sit(int sec) {
-        this.hp += sec;
-        System.out.println(this.name + "は、" + sec + "秒座った！");
-        System.out.println("HPが" + sec + "ポイント回復した");
-    }
-    public void slip() {
+    // public void sleep() {
+    //     this.hp = 100;
+    //     System.out.println(this.name + "は、眠って回復した！");
+    // }
+    // public void sit(int sec) {
+    //     this.hp += sec;
+    //     System.out.println(this.name + "は、" + sec + "秒座った！");
+    //     System.out.println("HPが" + sec + "ポイント回復した");
+    // }
+    public final void slip() {
         this.hp -= 5;
-        System.out.println(this.name + "は、転んだ！");
+        System.out.println(this.name + "は転んだ！");
         System.out.println("5のダメージ！");
     }
     public void run() {
-        System.out.println(this.name + "は、逃げ出した！");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした");
+        System.out.println(this.name + "は逃げ出した！");
     }
-    public Hero(String name) {
-        this.hp = 100;
-        this.name = name;
-    }
+    // public Hero(String name) {
+    //     this.hp = 100;
+    //     this.name = name;
+    // }
+    // public Hero() {
+    //     this("ダミー");
+    // }
     public Hero() {
-        this("ダミー");
+        System.out.println("Heroのコンストラクタが動作");
     }
 }
