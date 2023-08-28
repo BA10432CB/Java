d392c7f287c179775beafbd182e00251bf4810a8
BA10432CB <prest.0808.bass@gmail.com>
2023-08-28 19:58:12 +0900

    13章途中

--- programming/test/src/Hero.java
+++ programming/test/src/Hero.java
@@ -1,7 +1,28 @@
-public class Hero extends Character {
+public class Hero {
+    private int hp;
+    private String name;
+    Sword sword;
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void bye() {
+        System.out.println("勇者は別れを告げた");
+    }
+    private void die() {
+        System.out.println(this.name + "は死んでしまった");
+        System.out.println("GAME OVERです");
+    }
+    public void sleep() {
+        this.hp += 100;
+        System.out.println(this.name + "は、眠って回復した");
+    }
     public void attack(Matango m) {
         System.out.println(this.name + "の攻撃");
-        System.out.println("敵に10ポイントのダメージをあたえた");
-        m.hp -= 10;
+        m.hp -= 5;
+        System.out.println("敵に5ポイントのダメージ");
     }
 }
